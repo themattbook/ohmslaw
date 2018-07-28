3a8032924511bcbaab603632094ffb442a2afd73
Matthew Sweet <themattbook@gmail.com>
2018-07-28 13:32:02 -0500

    Update ohmscalculator.js

--- ohmscalculator.js
+++ ohmscalculator.js
@@ -2,14 +2,29 @@
 Ohm's Law Calculator
 Author: Matthew Sweet, KE0IMD
 Contact: ke0imd@gmail.com
-Description: Un-elegant Javascript
+Description: Probably the most inefficient means of calculating
+Ohm's law values using Javascript. Far from perfect.
+License: GNU General Public License 2.0
+https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
 */
 
 function ohmsCalc() {
+    /* 
+    Since the structure of Ohm's Law calculations only
+    require two values to proceed, this script will only check
+    for two values and calculate the remaining two.
+    */
+
+    // Declare our 4 inputs
     var volts = document.getElementById('voltage');
     var current = document.getElementById('current');
     var resistance = document.getElementById('resistance');
     var power = document.getElementById('power');
+
+    // Validation check
+    if (volts.value == "" & current.value == "" & resistance.value == "" & power.value == "") {
+        console.log("Error: Please provide two values.");
+    }
 
     // If only Power and Resistance are supplied, calculate Voltage and Current
     if (volts.value == "" & current.value == "") {
@@ -25,11 +40,11 @@ function ohmsCalc() {
         current.value = calcCurrent;
 
         // Log to Console
-        console.log("**********\nResults based on given values for R and P\n**********\n" + 
-        "Voltage: " + calcVolts + "V\n" + "Current: " + calcCurrent + "A\n" + "Resistance: "
-        + r + "Ω\n" + "Power: " + p + "W\n");
-    } 
-    
+        console.log("**********\nResults based on given values for R and P\n**********\n" +
+            "Voltage: " + calcVolts + "V\n" + "Current: " + calcCurrent + "A\n" + "Resistance: " +
+            r + "Ω\n" + "Power: " + p + "W\n");
+    }
+
     // If only Power and Current are supplied, calculate Voltage and Resistance
     else if (volts.value == "" & resistance.value == "") {
         var i = parseFloat(current.value);
@@ -45,9 +60,9 @@ function ohmsCalc() {
         resistance.value = Math.round(calcResistance);
 
         // Log to Console
-        console.log("**********\nResults based on given values for I and P\n**********\n" + 
-        "Voltage: " + calcVolts + "V\n" + "Current: " + i + "A\n" + "Resistance: "
-        + Math.round(calcResistance) + "Ω\n" + "Power: " + p + "W\n");
+        console.log("**********\nResults based on given values for I and P\n**********\n" +
+            "Voltage: " + calcVolts + "V\n" + "Current: " + i + "A\n" + "Resistance: " +
+            Math.round(calcResistance) + "Ω\n" + "Power: " + p + "W\n");
     }
 
     // If only Power and Voltage are supplied, calculate Current and Resistance
@@ -65,9 +80,9 @@ function ohmsCalc() {
         resistance.value = Math.round(calcResistance);
 
         // Log to Console
-        console.log("**********\nResults based on given values for E and P\n**********\n" + 
-        "Voltage: " + e + "V\n" + "Current: " + calcCurrent + "A\n" + "Resistance: "
-        + calcResistance + "Ω\n" + "Power: " + p + "W\n");
+        console.log("**********\nResults based on given values for E and P\n**********\n" +
+            "Voltage: " + e + "V\n" + "Current: " + calcCurrent + "A\n" + "Resistance: " +
+            calcResistance + "Ω\n" + "Power: " + p + "W\n");
     }
 
     // If only Voltage and Resistance are supplied, calculate Current and Power
@@ -85,9 +100,9 @@ function ohmsCalc() {
         power.value = calcPower;
 
         // Log to Console
-        console.log("**********\nResults based on given values for E and R\n**********\n" + 
-        "Voltage: " + e + "V\n" + "Current: " + calcCurrent + "A\n" + "Resistance: "
-        + r + "Ω\n" + "Power: " + calcPower + "W\n");
+        console.log("**********\nResults based on given values for E and R\n**********\n" +
+            "Voltage: " + e + "V\n" + "Current: " + calcCurrent + "A\n" + "Resistance: " +
+            r + "Ω\n" + "Power: " + calcPower + "W\n");
     }
 
     // If only Voltage and Current are supplied, calculate Resistance and Power
@@ -104,9 +119,9 @@ function ohmsCalc() {
         power.value = calcPower;
 
         // Log to Console
-        console.log("**********\nResults based on given values for E and I\n**********\n" + 
-        "Voltage: " + e + "V\n" + "Current: " + i + "A\n" + "Resistance: "
-        + calcResistance + "Ω\n" + "Power: " + calcPower + "W\n");
+        console.log("**********\nResults based on given values for E and I\n**********\n" +
+            "Voltage: " + e + "V\n" + "Current: " + i + "A\n" + "Resistance: " +
+            calcResistance + "Ω\n" + "Power: " + calcPower + "W\n");
     }
 
     // If only Resistance and Current are supplied, calculate Voltage and Power
@@ -124,8 +139,8 @@ function ohmsCalc() {
         power.value = calcPower;
 
         // Log to Console
-        console.log("**********\nResults based on given values for R and I\n**********\n" + 
-        "Voltage: " + calcVolts + "V\n" + "Current: " + i + "A\n" + "Resistance: "
-        + r + "Ω\n" + "Power: " + calcPower + "W\n");
+        console.log("**********\nResults based on given values for R and I\n**********\n" +
+            "Voltage: " + calcVolts + "V\n" + "Current: " + i + "A\n" + "Resistance: " +
+            r + "Ω\n" + "Power: " + calcPower + "W\n");
     }
 }
